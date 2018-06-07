c6ad7475a5986509d14c8cb23605b60c0c5e601a
Mike <mroopver@gmail.com>
2018-06-07 09:21:02 +0300

    new version of tables

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -27,6 +27,149 @@ const metacoin_artifacts = require('../../../../../build/contracts/DACOMain.json
 export class TableComponent {
 
 
+   data: Array<any> = [
+    {
+    "name": "Wing",
+    "email": "tellus.eu.augue@arcu.com",
+    "regDate": "2016-01-09T14:48:34-08:00",
+    "city": "Paglieta",
+    "age": 25
+  },
+    {
+      "name": "Whitney",
+      "email": "sed.dictum@Donec.org",
+      "regDate": "2017-01-23T20:09:52-08:00",
+      "city": "Bear",
+      "age": 32
+    },
+    {
+      "name": "Oliver",
+      "email": "mauris@Craslorem.ca",
+      "regDate": "2015-11-19T19:11:33-08:00",
+      "city": "Bruderheim",
+      "age": 31
+    },
+    {
+      "name": "Vladimir",
+      "email": "mi.Aliquam@Phasellus.net",
+      "regDate": "2015-11-02T07:59:34-08:00",
+      "city": "Andenne",
+      "age": 50
+    },
+    {
+      "name": "Maggy",
+      "email": "ligula@acorciUt.edu",
+      "regDate": "2017-02-25T15:42:17-08:00",
+      "city": "HomprŽ",
+      "age": 24
+     },
+     {
+       "name": "Wing",
+       "email": "tellus.eu.augue@arcu.com",
+       "regDate": "2016-01-09T14:48:34-08:00",
+       "city": "Paglieta",
+       "age": 25
+     },
+     {
+       "name": "Whitney",
+       "email": "sed.dictum@Donec.org",
+       "regDate": "2017-01-23T20:09:52-08:00",
+       "city": "Bear",
+       "age": 32
+     },
+     {
+       "name": "Oliver",
+       "email": "mauris@Craslorem.ca",
+       "regDate": "2015-11-19T19:11:33-08:00",
+       "city": "Bruderheim",
+       "age": 31
+     },
+     {
+       "name": "Vladimir",
+       "email": "mi.Aliquam@Phasellus.net",
+       "regDate": "2015-11-02T07:59:34-08:00",
+       "city": "Andenne",
+       "age": 50
+     },
+     {
+       "name": "Maggy",
+       "email": "ligula@acorciUt.edu",
+       "regDate": "2017-02-25T15:42:17-08:00",
+       "city": "HomprŽ",
+       "age": 24
+     },
+     {
+       "name": "Wing",
+       "email": "tellus.eu.augue@arcu.com",
+       "regDate": "2016-01-09T14:48:34-08:00",
+       "city": "Paglieta",
+       "age": 25
+     },
+     {
+       "name": "Whitney",
+       "email": "sed.dictum@Donec.org",
+       "regDate": "2017-01-23T20:09:52-08:00",
+       "city": "Bear",
+       "age": 32
+     },
+     {
+       "name": "Oliver",
+       "email": "mauris@Craslorem.ca",
+       "regDate": "2015-11-19T19:11:33-08:00",
+       "city": "Bruderheim",
+       "age": 31
+     },
+     {
+       "name": "Vladimir",
+       "email": "mi.Aliquam@Phasellus.net",
+       "regDate": "2015-11-02T07:59:34-08:00",
+       "city": "Andenne",
+       "age": 50
+     },
+     {
+       "name": "Maggy",
+       "email": "ligula@acorciUt.edu",
+       "regDate": "2017-02-25T15:42:17-08:00",
+       "city": "HomprŽ",
+       "age": 24
+     },
+     {
+       "name": "Wing",
+       "email": "tellus.eu.augue@arcu.com",
+       "regDate": "2016-01-09T14:48:34-08:00",
+       "city": "Paglieta",
+       "age": 25
+     },
+     {
+       "name": "Whitney",
+       "email": "sed.dictum@Donec.org",
+       "regDate": "2017-01-23T20:09:52-08:00",
+       "city": "Bear",
+       "age": 32
+     },
+     {
+       "name": "Oliver",
+       "email": "mauris@Craslorem.ca",
+       "regDate": "2015-11-19T19:11:33-08:00",
+       "city": "Bruderheim",
+       "age": 31
+     },
+     {
+       "name": "Vladimir",
+       "email": "mi.Aliquam@Phasellus.net",
+       "regDate": "2015-11-02T07:59:34-08:00",
+       "city": "Andenne",
+       "age": 50
+     },
+     {
+       "name": "Maggy",
+       "email": "ligula@acorciUt.edu",
+       "regDate": "2017-02-25T15:42:17-08:00",
+       "city": "HomprŽ",
+       "age": 24
+     }
+   ];
+
   @Input() ng2TableData: Array<any>;
   @Input() columns: Array<any> ;
 
--- src/app/shared/components/table/table.module.ts
+++ src/app/shared/components/table/table.module.ts
@@ -8,7 +8,9 @@ import { UtilModule } from '../../../util/util.module';
 
 import { PaginationModule } from 'ngx-bootstrap';
 import { Ng2TableModule } from 'ng2-table';
+import { DataTableModule } from 'angular2-datatable';
 import { WidgetModule } from '../../../layout/widget/widget.module';
+import { SearchPipe } from './pipes/search-pipe';
 
 
 
@@ -16,9 +18,10 @@ import { WidgetModule } from '../../../layout/widget/widget.module';
   imports: [CommonModule,
     PaginationModule.forRoot(),
     WidgetModule,
-    Ng2TableModule
+    Ng2TableModule,
+    DataTableModule
   ],
-  declarations: [TableComponent],
+  declarations: [TableComponent, SearchPipe],
   exports: [TableComponent, 
     CommonModule, FormsModule]
 })
